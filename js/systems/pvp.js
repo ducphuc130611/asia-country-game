bee07405cda1ba0855efb1eec3777233f82fab8b
ducphuc130611 <ducphuc130611@gmail.com>
2026-08-22 13:32:09 +0700

    v3.5.5: defer PvP to V4

--- js/systems/pvp.js
+++
@@ -1,3 +0,0 @@
-import {api} from '../core/api.js';
-export async function findMatch(token){return api.match(token);}
-export async function submitResult(token,matchId,winnerId){return api.result(token,matchId,winnerId);}
